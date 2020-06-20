c26343a5f6962103d5fe63d175856af02347d67a
hanchanghun <ehwjs1914@ajou.ac.kr>
2020-06-20 12:46:18 +0900

    end trigger

--- notebooks/queries/4.view_query.sql
+++
@@ -1,8 +0,0 @@
-create view VegProduct as select prdlstReportNo, P.*
-from FoodInfo F, VegPrdKind P
-where F.prdKind = P.prdKind;
-
-create view AllergyProduct as select prdlstReportNo, P.*
-from FoodInfo F, AllergyPrdKind P
-where F.prdKind = P.prdKind;
-

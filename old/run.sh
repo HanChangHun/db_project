879a3a67a4b4058599bfb4659f06fe0d1fee263e
hanchanghun <ehwjs1914@ajou.ac.kr>
2020-06-21 13:52:08 +0900

    trivial change

--- old/run.sh
+++ old/run.sh
@@ -22,7 +22,7 @@ echo "num_cpu: $CPU_NUM"
 echo "momory size: $MEMORY_SIZE"
 echo "momory swap size: $MEMORY_SWAP_SIZE"
 
-docker run --name db40 -e POSTGRES_PASSWORD=1234 -dit postgres \
+docker run --name db40 -e POSTGRES_PASSWORD=1234 -dit 40db \
            -m ${MEMORY_SIZE}G \
            --memory-swap ${MEMORY_SWAP_SIZE}G \
            --cpus ${CPU_SHARES}
